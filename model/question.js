d7cd747bcccc32660e260f2236074f891ba1ac3f
00sukhjeet00 <sukhjeet20002@gmail.com>
2021-10-17 17:39:26 +0530

    new commit

--- model/question.js
+++ model/question.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const questionSchema = new Schema({
     name: { type: String, required: true },
-    code: { type: String, required: true ,maxlength:6,minlength:6},
+    code: { type: String, required: true ,maxlength:6,minlength:4,unique:true},
     desc: { type: String, required: true },
     author:{type:schema.Type.ObjectId,ref='user'},
 })
--- model/user.js
+++ model/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    emial: { type: String, required: true },
+    username:{type:String,required:true,unique:true},
+    email: { type: String, required: true ,unique:true},
     password: { type: String, required: true},
     isAdmin: { type: Boolean, default: false },
     isSupAdmin: { type: Boolean, default: false },
